c0b5df96052054c8bc4a06373c20060e52c29064
tbshno <33148885+tbshno@users.noreply.github.com>
2019-02-16 05:41:12 +0700

    Create Lingkaran.java

---
+++ bangundatar/Lingkaran.java
@@ -0,0 +1,28 @@
+package bangundatar;
+
+// Created on Feb 16, 2019 4:59:43 AM
+
+public class Lingkaran implements BangunDatar {
+  
+  private double pi = 3.1415;
+  
+  Lingkaran(){}
+  
+  @Override
+  public double luas(int s1, int s2) {
+    return 0;
+  }
+  
+  @Override
+  public double keliling(int s1, int s2) {
+    return 0;
+  }
+  
+  public double luas(int r){ //metode overload
+    return (pi * r * r);
+  }
+  
+  public double keliling(int r){ //methode overload
+    return (2 * pi * r);
+  }
+}
