1d4b15cd9033a439c006d385785f0474041acb73
tbshno <33148885+tbshno@users.noreply.github.com>
2019-02-16 06:22:44 +0700

    Update SegitigaSiku.java

--- bangundatar/SegitigaSiku.java
+++ bangundatar/SegitigaSiku.java
@@ -7,12 +7,12 @@ public class SegitigaSiku implements BangunDatar {
     SegitigaSiku(){}
  
     @Override
-    public double luas(int s1, int s2) {
+    public double luas(double s1, double s2) {
         return (0.5 * s1 * s2);
     }
  
     @Override
-    public double keliling(int s1, int s2) {
+    public double keliling(double s1, double s2) {
         return (s1 + s2 + Math.sqrt((s1*s1) + (s2*s2)));
     }
 }
