c40ac3751289b0a56b217709ff68b651679ba6cf
tbshno <33148885+tbshno@users.noreply.github.com>
2019-02-16 06:14:42 +0700

    Create BelahKetupat.java

---
+++ bangundatar/BelahKetupat.java
@@ -0,0 +1,16 @@
+package bangundatar;
+
+public class BelahKetupat implements BangunDatar{
+  public double luas(int s1, int s2){
+    return 0.0;
+  }
+  public double keliling(int s1, int s2){
+    return 0.0;
+  }
+  public double luas(double s, double d1, double d2){
+     return 0.5*d1*d2;
+  }
+  public double keliling(double s, double d1, double d2){
+    return 4.0*s;
+  }
+}
