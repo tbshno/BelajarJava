cfec893271905da6b0854c1ff450044ed948d01b
tbshno <33148885+tbshno@users.noreply.github.com>
2019-02-16 06:23:39 +0700

    Update Lingkaran.java

--- bangundatar/Lingkaran.java
+++ bangundatar/Lingkaran.java
@@ -9,20 +9,20 @@ public class Lingkaran implements BangunDatar {
   Lingkaran(){}
   
   @Override
-  public double luas(int s1, int s2) {
+  public double luas(double s1, double s2) {
     return 0;
   }
   
   @Override
-  public double keliling(int s1, int s2) {
+  public double keliling(double s1, double s2) {
     return 0;
   }
   
-  public double luas(int r){ //metode overload
+  public double luas(double r){ //metode overload
     return (pi * r * r);
   }
   
-  public double keliling(int r){ //methode overload
+  public double keliling(double r){ //methode overload
     return (2 * pi * r);
   }
 }
