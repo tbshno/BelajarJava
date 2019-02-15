cd5cfbbfb49c90cddb69eeb78959a19266630d02
tbshno <33148885+tbshno@users.noreply.github.com>
2019-02-16 06:20:41 +0700

    Update BangunDatar.java

--- bangundatar/BangunDatar.java
+++ bangundatar/BangunDatar.java
@@ -3,6 +3,6 @@ package bangundatar;
 // Created on Feb 16, 2019 4:52:27 AM
 
 public interface BangunDatar {
-    public double luas(int s1, int s2);
-    public double keliling(int s1, int s2);
+    public double luas(double s1, double s2);
+    public double keliling(double s1, double s2);
 }
